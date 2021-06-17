db6c93db04d0fbbda7c4e736b57ab2df615225e3
huuhan2507@gmail.com <you@example.com>
2021-06-17 08:53:10 +0700

    update_Fe

--- C1120G1-Sprint2-FE/src/app/module/employee/employee-sales-tickets-management/employee-sales-tickets-management.module.ts
+++ C1120G1-Sprint2-FE/src/app/module/employee/employee-sales-tickets-management/employee-sales-tickets-management.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeSalesTicketsManagementRoutingModule } from './employee-sales-tickets-management-routing.module';
-import { SelectMovieComponent } from './select-movie/select-movie.component';
-import { SelectSeatComponent } from './select-seat/select-seat.component';
+import { MovieSelectionComponent } from './movie-selection/movie-selection.component';
+import { SeatSelectionComponent } from './seat-selection/seat-selection.component';
 import { ConfirmSaleTicketComponent } from './confirm-sale-ticket/confirm-sale-ticket.component';
 import { InforSaleTicketComponent } from './infor-sale-ticket/infor-sale-ticket.component';
 
 
 @NgModule({
-  declarations: [SelectMovieComponent, SelectSeatComponent, ConfirmSaleTicketComponent, InforSaleTicketComponent],
+  declarations: [MovieSelectionComponent, SeatSelectionComponent, ConfirmSaleTicketComponent, InforSaleTicketComponent],
   imports: [
     CommonModule,
     EmployeeSalesTicketsManagementRoutingModule
---
+++ C1120G1-Sprint2-FE/src/app/module/employee/employee-sales-tickets-management/movie-selection/movie-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-movie',
+  templateUrl: './movie-selection.component.html',
+  styleUrls: ['./movie-selection.component.css']
+})
+export class MovieSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
