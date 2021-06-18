3cbc0d25b4e56f79c11a164a03553b6acb9c0e22
vintt <nttuongvi.36@gmail.com>
2021-06-18 16:37:32 +0700

    ViNTT update FE

--- C1120G1-Sprint2-FE/src/app/module/main/main.module.ts
+++ C1120G1-Sprint2-FE/src/app/module/main/main.module.ts
@@ -4,13 +4,15 @@ import {MainHomePageComponent} from './main-home-page/main-home-page.component';
 import {MainHeaderComponent} from './main-header/main-header.component';
 import {MainFooterComponent} from './main-footer/main-footer.component';
 import {MainContentComponent} from './main-content/main-content.component';
+import {AppRoutingModule} from '../../app-routing.module';
 
 
 
 @NgModule({
   declarations: [MainHomePageComponent, MainHeaderComponent, MainFooterComponent, MainContentComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        AppRoutingModule
+    ]
 })
 export class MainModule { }
