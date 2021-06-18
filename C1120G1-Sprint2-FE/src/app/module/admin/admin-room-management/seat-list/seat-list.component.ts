4e9ddc0dec8cbde2b4822fbe49cc86988962cb8f
minhtuan94 <minhhoantuan21@gmail.com>
2021-06-18 17:27:21 +0700

    tuan update 18/6

--- C1120G1-Sprint2-FE/src/app/module/admin/admin-room-management/seat-list/seat-list.component.ts
+++ C1120G1-Sprint2-FE/src/app/module/admin/admin-room-management/seat-list/seat-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Row} from '../../../../model/row';
+import {Columns} from '../../../../model/columns';
+import {Seat} from '../../../../model/seat';
 
 @Component({
   selector: 'app-seat-list',
@@ -7,24 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeatListComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public rowList: Row[] = [
+    new Row(1, 'A'),
+    new Row(2, 'B'),
+    new Row(3, 'C'),
+    new Row(4, 'D'),
+    new Row(5, 'E'),
+    new Row(6, 'F'),
+    new Row(7, 'G'),
+  ];
+
+  public columnList: Columns[] = [
+    new Columns(1, '1'),
+    new Columns(2, '2'),
+    new Columns(3, '3'),
+    new Columns(4, '4'),
+    new Columns(5, '5'),
+    new Columns(6, '6'),
+    new Columns(7, '7'),
+  ];
+
+
+  constructor() {
   }
 
-  counter(number: number) {
-    return new Array(number)
+  ngOnInit(): void {
   }
 
   changeColor(x: number, y: number) {
     console.log(x);
     console.log(y);
-    let seat = document.getElementById(x.toString() + y.toString());
-    console.log(x.toString() + y.toString());
-    if (seat != null) {
-      document.getElementById(x.toString() + y.toString()).style.backgroundColor = 'green';
-    }
-
+    let row = document.getElementById(x.toString());
+    let col = document.getElementById(y.toString());
+    console.log(row);
   }
 
 }
+
