31858cdc1ed086a7fc8669d6c32ba5b3f977606a
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2021-06-20 23:41:41 +0700

    ThuanNN FE login-v1

--- C1120G1-Sprint2-FE/src/app/app.module.ts
+++ C1120G1-Sprint2-FE/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {AdminModule} from './module/admin/admin.module';
 import {EmployeeModule} from './module/employee/employee.module';
 import {MemberModule} from './module/member/member.module';
 import {MainModule} from './module/main/main.module';
 import {SecurityModule} from './module/security/security.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireStorageModule} from '@angular/fire/storage';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {environment} from '../environments/environment';
-import { LoadingComponent } from './module/loading/loading.component';
+import {LoadingComponent} from './module/loading/loading.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -31,9 +32,11 @@ import { LoadingComponent } from './module/loading/loading.component';
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- C1120G1-Sprint2-FE/src/app/module/security/login/login.component.ts
+++ C1120G1-Sprint2-FE/src/app/module/security/login/login.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {SecurityService} from '../../../service/security/security.service';
+import {Router} from '@angular/router';
+import {AuthLogin} from '../../../model/authLogin';
+import {TokenStorageService} from '../../../service/security/token-storage.service';
+
+// import {MainHeaderComponent} from '../../main/main-header/main-header.component';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  textType: boolean = false;
+  authLogin: AuthLogin;
+  role: string = '';
+  username: string;
+  wrongAccountMessage: string;
+  lockedAccountMessage: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private securityService: SecurityService,
+              private tokenStorageService: TokenStorageService,
+              private router: Router,
+              // private headerComponent: MainHeaderComponent
+  ) {
   }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9 ]+$'),
+        Validators.minLength(6), Validators.maxLength(45)]],
+      password: ['', [Validators.required,
+        Validators.minLength(6), Validators.maxLength(45)]],
+      remember: ['']
+    });
+
+    if (this.tokenStorageService.getToken()) {
+      const user = this.tokenStorageService.getUser();
+      this.securityService.isLoggedIn = true;
+      this.role = user.authorities[0].authority;
+      this.username = user.username;
+    }
+  }
+
+  get formUsername() {
+    return this.form.get('username');
+  }
+
+  get formPassword() {
+    return this.form.get('password');
+  }
+
+  public login(authLogin) {
+    this.securityService.login(authLogin).subscribe(
+      data => {
+        // if (data.account.accountStatus.accountStatusName !== 'Đã khoá') {
+          if (this.form.value.remember) {
+            this.tokenStorageService.saveTokenLocal(data.accessToken);
+            this.tokenStorageService.saveUserLocal(data);
+          } else {
+            this.tokenStorageService.saveTokenSession(data.accessToken);
+            this.tokenStorageService.saveUserLocal(data);
+          }
+
+          this.securityService.isLoggedIn = true;
+          this.username = this.tokenStorageService.getUser().username;
+          // this.role = this.tokenStorageService.getUser().authorities[0].authority;
+          this.form.reset();
+          console.log('Login Success');
+          // this.headerComponent.ngOnInit();
+          this.router.navigateByUrl('/'); // back to homepage
+        // } else {
+        //   console.log('Account has been locked function on LoginComponent');
+        //   this.lockedAccountMessage = 'Tài khoản đã bị khoá!';
+        //   this.securityService.isLoggedIn = false;
+        // }
+      },
+      err => {
+        console.log('Error at login function on LoginComponent');
+        this.wrongAccountMessage = 'Username/Mật khẩu không đúng. Vui lòng thử lại!';
+        this.securityService.isLoggedIn = false;
+      }
+    );
+  }
+
+  submitForm() {
+    this.authLogin = new AuthLogin(this.formUsername.value, this.formPassword.value);
+    this.login(this.authLogin);
+  }
+
+  toggleShowHide() {
+    this.textType = !this.textType;
+  }
 }
