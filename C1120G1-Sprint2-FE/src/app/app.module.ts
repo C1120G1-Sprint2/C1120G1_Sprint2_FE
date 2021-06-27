521214d8587d49310a46e333eaa86154fcadaf27
Tuong Vi Nguyen <nttuongvi.36@gmail.com>
2021-06-27 21:37:47 +0700

    ViNTT update FE

--- C1120G1-Sprint2-FE/src/app/app.module.ts
+++ C1120G1-Sprint2-FE/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AdminModule} from './module/admin/admin.module';
-import {EmployeeModule} from './module/employee/employee.module';
-import {MemberModule} from './module/member/member.module';
-import {MainModule} from './module/main/main.module';
-import {SecurityModule} from './module/security/security.module';
+import { AdminModule } from './module/admin/admin.module';
+import { EmployeeModule } from './module/employee/employee.module';
+import { MemberModule } from './module/member/member.module';
+import { MainModule } from './module/main/main.module';
+import { SecurityModule } from './module/security/security.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFireStorageModule} from '@angular/fire/storage';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-import {environment} from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 import { LoadingComponent } from './module/loading/loading.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { LoadingComponent } from './module/loading/loading.component';
     AppRoutingModule,
     AdminModule,
     EmployeeModule,
+    HttpClientModule,
     MemberModule,
     MainModule,
     SecurityModule,
--- C1120G1-Sprint2-FE/src/app/module/main/main.module.ts
+++ C1120G1-Sprint2-FE/src/app/module/main/main.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MainHomePageComponent} from './main-home-page/main-home-page.component';
 import {MainHeaderComponent} from './main-header/main-header.component';
 import {MainFooterComponent} from './main-footer/main-footer.component';
 import {MainContentComponent} from './main-content/main-content.component';
 import {AppRoutingModule} from '../../app-routing.module';
-
+import {MovieTrailerComponent} from './movie-trailer/movie-trailer.component';
+import {MainSearchComponent} from './main-search/main-search.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [MainHomePageComponent, MainHeaderComponent, MainFooterComponent, MainContentComponent],
-    imports: [
-        CommonModule,
-        AppRoutingModule
-    ]
+  declarations: [MainHomePageComponent, MainHeaderComponent, MainFooterComponent, MainContentComponent, MovieTrailerComponent, MainSearchComponent],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
-export class MainModule { }
+export class MainModule {
+}
