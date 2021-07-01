2d291e083edf249fa5483cf37fe6413f6bc4261b
Tuong Vi Nguyen <nttuongvi.36@gmail.com>
2021-07-01 13:48:31 +0700

    ViNTT update FE

--- C1120G1-Sprint2-FE/src/app/module/main/main.module.ts
+++ C1120G1-Sprint2-FE/src/app/module/main/main.module.ts
@@ -8,10 +8,14 @@ import {AppRoutingModule} from '../../app-routing.module';
 import {MovieTrailerComponent} from './movie-trailer/movie-trailer.component';
 import {MainSearchComponent} from './main-search/main-search.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { MovieCornerComponent } from './movie-corner/movie-corner.component';
+import { MainEventsComponent } from './main-events/main-events.component';
+import { MainInstructionsComponent } from './main-instructions/main-instructions.component';
+import { MainPolicyComponent } from './main-policy/main-policy.component';
 
 
 @NgModule({
-  declarations: [MainHomePageComponent, MainHeaderComponent, MainFooterComponent, MainContentComponent, MovieTrailerComponent, MainSearchComponent],
+  declarations: [MainHomePageComponent, MainHeaderComponent, MainFooterComponent, MainContentComponent, MovieTrailerComponent, MainSearchComponent, MovieCornerComponent, MainEventsComponent, MainInstructionsComponent, MainPolicyComponent],
   imports: [
     CommonModule,
     AppRoutingModule,
